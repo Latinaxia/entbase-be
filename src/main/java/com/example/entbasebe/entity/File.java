2e49a676911d45bc4c30e23a84fe1c13734fd83c
Latinaxia <3276327856@qq.com>
2024-05-29 20:41:20 +0800

    初始化了File，Folder，User的baen类，其中有些注释不用的话可以屏蔽掉

--- src/main/java/com/example/entbasebe/entity/File.java
+++ src/main/java/com/example/entbasebe/entity/File.java
@@ -1,11 +1,40 @@
 package com.example.entbasebe.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class File {
+@TableName("file")
+@EqualsAndHashCode(callSuper = false)// 重写equals和hashCode方法，保证两个对象相同的属性值也相同
+@Accessors(chain = true)// 支持链式调用，setter返回的将为this值而不是void，没有这个习惯可以将chain设为false
+public class File implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "file_id", type = IdType.AUTO)
+    Integer file_id;
+
+    String file_name;
+
+    String file_path;
+
+    Integer fold_id;
+
+    Integer user_id;
+
+    LocalDateTime creat_time;
+
+    LocalDateTime update_time;
 }
--- src/main/java/com/example/entbasebe/entity/User.java
+++ src/main/java/com/example/entbasebe/entity/User.java
@@ -1,11 +1,45 @@
 package com.example.entbasebe.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+@TableName("user")
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "user_id", type = IdType.AUTO)
+    Integer user_id;
+
+    String user_name;
+
+    String user_password;
+
+    String user_email;
+
+    String is_admin;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
