eda73bff8824d23bc38a1a21709bde5efb3bb6d8
Latinaxia <3276327856@qq.com>
2024-05-29 21:08:43 +0800

    定义了一个全局异常处理类WebExceptionAdvice，当发生RuntimeException时，会记录异常信息并返回一个描述服务器异常的结果。这有助于统一处理应用程序中的异常情况，并向客户端提供一致的错误响应。 编写了一个Jwt令牌的生成和解析方法类，使用了HS256算法，其中signKey为"entbase-be666"

---
+++ src/main/java/com/example/entbasebe/Config/WebExceptionAdvice.java
@@ -0,0 +1,20 @@
+package com.example.entbasebe.Config;
+
+import com.example.entbasebe.Utils.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/*定义了一个全局异常处理类WebExceptionAdvice，
+发生RuntimeException时，会记录异常信息并返回一个描述服务器异常的结果。
+ 这有助于统一处理应用程序中的异常情况，并向客户端提供一致的错误响应。
+ */
+@Slf4j
+@RestControllerAdvice
+public class WebExceptionAdvice {
+    @ExceptionHandler(RuntimeException.class)
+    public Result handleRuntimeException(RuntimeException e) {
+        log.error(e.toString(), e);
+        return Result.fail("服务器异常");
+    }
+}
---
+++ src/main/java/com/example/entbasebe/Utils/JwtUtils.java
@@ -0,0 +1,41 @@
+package com.example.entbasebe.Utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.Map;
+
+public class JwtUtils {
+
+    private static final String signKey = "entbase-be666";
+    private static final Long expire = 43200000L;//有效期12小时
+
+    /**
+     * 生成JWT令牌
+     * @param claims JWT第二部分负载 payload 中存储的内容
+     * @return
+     */
+    public static String generateJwt(Map<String, Object> claims){
+        String jwt = Jwts.builder()
+                .addClaims(claims)
+                .signWith(SignatureAlgorithm.HS256, signKey)//signKey太短的话会报错，用长一点的字符串
+                .setExpiration(new Date(System.currentTimeMillis() + expire))
+                .compact();
+        return jwt;
+    }
+
+    /**
+     * 解析JWT令牌
+     * @param jwt JWT令牌
+     * @return JWT第二部分负载 payload 中存储的内容
+     */
+    public static Claims parseJWT(String jwt){
+        Claims claims = Jwts.parser()
+                .setSigningKey(signKey)
+                .parseClaimsJws(jwt)
+                .getBody();
+        return claims;
+    }
+}
