a84bc1a60c5d9f5c52a5715805d42ce107d4ed59
Zhousiru <i@zhousiru.com>
2024-06-02 15:48:27 +0800

    添加 Dockerfile 与 Docker Compose 文件

---
+++ entbase.sql
@@ -0,0 +1,77 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80026 (8.0.26)
+ Source Host           : localhost:3306
+ Source Schema         : entbase
+
+ Target Server Type    : MySQL
+ Target Server Version : 80026 (8.0.26)
+ File Encoding         : 65001
+
+ Date: 31/05/2024 21:39:10
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for bucket
+-- ----------------------------
+DROP TABLE IF EXISTS `bucket`;
+CREATE TABLE `bucket`  (
+  `bucket_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `bucket_space` int NOT NULL DEFAULT 2048,
+  `is_public` varchar(4) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0',
+  PRIMARY KEY (`bucket_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for file
+-- ----------------------------
+DROP TABLE IF EXISTS `file`;
+CREATE TABLE `file`  (
+  `file_id` int NOT NULL AUTO_INCREMENT,
+  `file_path` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `fold_id` int NULL DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `file_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `creat_time` datetime NULL DEFAULT NULL,
+  `update_time` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`file_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for folder
+-- ----------------------------
+DROP TABLE IF EXISTS `folder`;
+CREATE TABLE `folder`  (
+  `fold_id` int NOT NULL AUTO_INCREMENT,
+  `fold_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `fold_path` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `father_id` int NULL DEFAULT NULL,
+  `creat_time` datetime NULL DEFAULT NULL,
+  `update_time` datetime NULL DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `is_bucket` int NOT NULL DEFAULT 0,
+  PRIMARY KEY (`fold_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `user_password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `is_admin` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `user_email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `icon` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
